71c332137692259b8969eaa2e79fa7c44dc4ca8b
aumnatthacha <aummumdaeng@gmail.com>
2023-10-26 10:08:01 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,18 @@
 /* eslint-disable no-unused-vars */
 import * as React from 'react';
-import { Link, useNavigate  } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
 
 const pages = ['Add', 'Login'];
-const settings = ['/Profile', '/Logout'];
 const userRole = 'admin'; // แทนค่านี้ด้วยบทบาทของผู้ใช้จริง
 
 function Navbar() {
@@ -43,10 +37,17 @@ function Navbar() {
   };
 
   const handleLogout = () => {
-    // ทำการลอกเอาท์ผู้ใช้ออกจากระบบและเปลี่ยนเส้นทางไปยังหน้า "/login"
-    // เช่น เคลียร์ข้อมูลการเข้าสู่ระบบ, ล้างคุกกี้, ล้างค่าแปล, เปลี่ยนเส้นทางหรือทำการออกจากระบบที่เซิร์ฟเวอร์
-    history.push('/login');
+    if (isLoggedIn) {
+      navigate('/login');
+    }
   };
+
+  const handleProfile = () => {
+    if (isLoggedIn) {
+      navigate('/profile');
+    }
+  };
+
 
   // ตรวจสอบว่าผู้ใช้เข้าสู่ระบบหรือไม่
   const isLoggedIn = userRole === 'admin';
@@ -55,67 +56,13 @@ function Navbar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component={Link} to="/" /* ใช้ Link ไปหน้า Home */
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
                 component={Link} to={userRole === 'admin' && page === 'Add' ? '/Add' : page === 'Login' ? '/Login' : '/'}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ mx: 2, my: 2, color: 'white', display: 'block' }}
               >
                 {page}
               </Button>
@@ -123,40 +70,16 @@ function Navbar() {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-          
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                // ตรวจสอบการเข้าสู่ระบบก่อนที่จะแสดงลิง์ "Profile" และ "Logout"
-                isLoggedIn ? (
-                  setting === '/Logout' ? (
-                    <div onClick={handleLogout}>
-                      <Link to="/Login" style={{ textDecoration: 'none' }}>
-                        <Typography textAlign="center">Logout</Typography>
-                      </Link>
-                    </div>
-                  ) : (
-                    <Link to={setting} style={{ textDecoration: 'none' }}>
-                      <Typography textAlign="center">{setting === '/Profile' ? 'Profile' : setting}</Typography>
-                    </Link>
-                  )
-                ) : null
-              ))}
-            </Menu>
+            {isLoggedIn && (userRole === 'user' || userRole === 'admin') ? (
+              <div>
+                <Button onClick={handleProfile} component={Link} to="/Profile" sx={{ mx: 1, color: 'white' }}>
+                  Profile
+                </Button>
+                <Button onClick={handleLogout} component={Link} to="/login" sx={{ mx: 1, color: 'white' }}>
+                  Logout
+                </Button>
+              </div>
+            ) : null}
           </Box>
         </Toolbar>
       </Container>
