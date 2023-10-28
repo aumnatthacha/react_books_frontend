eb1ec453b2882962df6200bf362d93ade4310531
aumnatthacha <aummumdaeng@gmail.com>
2023-10-28 21:08:34 +0700

    home,navber

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,11 @@ import { Link, useNavigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 
-const pages = ['Add', 'Login'];
+const pages = ['Home', 'Add', 'Login'];
 const userRole = 'admin'; // แทนค่านี้ด้วยบทบาทของผู้ใช้จริง
 
 function Navbar() {
@@ -62,69 +56,66 @@ function Navbar() {
   const isLoggedIn = userRole === 'admin';
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: 'DimGrey' }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                component={Link}
-                to={userRole === 'admin' && page === 'Add' ? '/Add' : page === 'Login' ? '/Login' : '/'}
-                sx={{ mx: 2, my: 2, color: 'white', display: 'block', backgroundColor: 'DimGrey' }}
-              >
-                {page}
-              </Button>
-
-            ))}
-          </Box>
-          <Box
+    <AppBar position="fixed" sx={{ backgroundColor: 'DimGrey', top: 0, left: 0, right: 0 }}>
+      <Toolbar disableGutters>
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              component={Link}
+              to={userRole === 'admin' && page === 'Add' ? '/Add' : page === 'Login' ? '/Login' : '/'}
+              sx={{ mx: 2, my: 2, color: 'white', display: 'block', backgroundColor: 'DimGrey' }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            ml: 'auto',
+            backgroundColor: 'LightGray',
+            borderRadius: 2,
+            padding: '2px 6px',
+            marginRight: !isLoggedIn ? '0' : '10px', // เพิ่มการกำหนดค่า margin-right เมื่อ Profile และ Logout ไม่แสดง
+          }}
+        >
+          <SearchIcon sx={{ color: 'white' }} />
+          <InputBase
+            placeholder="Search..."
             sx={{
-              display: 'flex',
-              alignItems: 'center',
-              ml: 'auto',
-              backgroundColor: 'LightGray',
-              borderRadius: 2,
-              padding: '2px 6px',
+              ml: 1,
+              flex: 1,
+              p: 0.75,
+              color: 'black',
+              '&::placeholder': {
+                color: 'white',
+              },
             }}
-          >
-            <SearchIcon sx={{ color: 'white' }} />
-            <InputBase
-              placeholder="Search..."
-              sx={{
-                ml: 1,
-                flex: 1,
-                p: 0.75,
-                color: 'black',
-                '&::placeholder': {
-                  color: 'white',
-                },
-              }}
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              onKeyPress={(e) => {
-                if (e.key === 'Enter') {
-                  handleSearch();
-                }
-              }}
-            />
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            {isLoggedIn && (userRole === 'user' || userRole === 'admin') ? (
-              <div>
-                <Button onClick={handleProfile} component={Link} to="/Profile" sx={{ mx: 1, color: 'white' }}>
-                  Profile
-                </Button>
-                <Button onClick={handleLogout} component={Link} to="/login" sx={{ mx: 1, color: 'white' }}>
-                  Logout
-                </Button>
-              </div>
-            ) : null}
-          </Box>
-        </Toolbar>
-      </Container>
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                handleSearch();
+              }
+            }}
+          />
+        </Box>
+        <Box sx={{ flexGrow: 0 }}>
+          {isLoggedIn ? (
+            <div>
+              <Button onClick={handleProfile} component={Link} to="/Profile" sx={{ mx: 1, color: 'white' }}>
+                Profile
+              </Button>
+              <Button onClick={handleLogout} component={Link} to="/login" sx={{ mx: 1, color: 'white' }}>
+                Logout
+              </Button>
+            </div>
+          ) : null}
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,14 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { booksData } from './booksData';
 import { useNavigate } from 'react-router-dom';
+import Collapse from '@mui/material/Collapse';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Home = () => {
   const [books, setBooks] = useState(booksData);
   const [selectedBook, setSelectedBook] = useState(null);
   const navigate = useNavigate();
+  const isAdmin = true; // ตรวจสอบสิทธิ์ "admin" ที่นี่
 
   const handleEdit = (book) => {
     setSelectedBook(book);
@@ -23,75 +26,76 @@ const Home = () => {
     setBooks(updatedBooks);
   };
 
-  const groupSize = 5; // จำนวนรายการในแต่ละกลุ่ม
-
-  const groupedBooks = books.reduce((result, item, index) => {
-    const groupIndex = Math.floor(index / groupSize);
-    if (!result[groupIndex]) {
-      result[groupIndex] = [];
-    }
-    result[groupIndex].push(item);
-    return result;
-  }, []);
+  const handleExpandClick = (book) => {
+    book.expanded = !book.expanded;
+    setBooks([...books]);
+  };
 
   const handleCancel = () => {
-    navigate('/')
+    navigate('/');
   };
 
   return (
     <div>
-      {groupedBooks.map((group, groupIndex) => (
-        <div
-          key={groupIndex}
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center', // จัดให้อยู่ตรงกลาง
-            marginTop: '6rem', // ระยะห่างด้านบน
+      {books.map((book) => (
+        <Card
+          key={book.id}
+          sx={{
+            maxWidth: 167,
+            width: '23%',
+            marginBottom: '4rem', // ระยะห่างระหว่างรายการ
+            marginLeft: '1.5rem', // ระยะห่างด้านซ้าย
+            marginRight: '1.5rem', // ระยะห่างด้านขวา
+            borderRadius: '1px',
           }}
+          className="books-card"
         >
-          {group.map((book) => (
-            <Card
-              key={book.id}
-              sx={{
-                maxWidth: 167,
-                width: '23%',
-                marginBottom: '4rem', // ระยะห่างระหว่างรายการ
-                marginLeft: '1.5rem', // ระยะห่างด้านซ้าย
-                marginRight: '1.5rem', // ระยะห่างด้านขวา
-                borderRadius: '1px',
-              }}
-              className="books-card"
+          <CardMedia
+            component="img"
+            style={{
+              maxWidth: '167px',
+              maxHeight: '250px',
+            }}
+            alt={book.name}
+            image={book.imageURL}
+            sx={{ objectFit: 'cover' }}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {book.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {book.type}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            {!isAdmin && (
+              <Button onClick={() => navigate('/update/' + book.id)}>
+                <i className="bi bi-arrow-repeat"></i> แก้ไข
+              </Button>
+            )}
+            {!isAdmin && (
+              <Button onClick={() => handleDelete(book.id)}>
+                <i className="bi bi-trash"></i> ลบ
+              </Button>
+            )}
+            <Button
+              onClick={() => handleExpandClick(book)}
+              aria-expanded={book.expanded}
             >
-              <CardMedia
-                component="img"
-                style={{
-                  maxWidth: '167px',
-                  maxHeight: '250px',
-                }}
-                alt={book.name}
-                image={book.imageURL}
-                sx={{ objectFit: 'cover' }}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {book.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {book.type}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button onClick={() => navigate('/update/' + book.id)}>
-                  <i className="bi bi-arrow-repeat"></i> แก้ไข
-                </Button>
-                <Button onClick={() => handleDelete(book.id)}>
-                  <i className="bi bi-trash"></i> ลบ
-                </Button>
-              </CardActions>
-            </Card>
-          ))}
-        </div>
+              {book.expanded ? 'ยุบ' : 'แสดงรายละเอียด'}
+              <ExpandMoreIcon />
+            </Button>
+          </CardActions>
+          <Collapse in={book.expanded}>
+            <CardContent>
+              <Typography paragraph>ข้อมูลเพิ่มเติมเกี่ยวกับหนังสือ:</Typography>
+              <Typography>
+                ที่นี่คุณสามารถเพิ่มข้อมูลเพิ่มเติมเกี่ยวกับหนังสือของคุณ หากคุณมีข้อมูลเพิ่มเติมที่คุณต้องการแสดงให้ผู้ใช้เห็น.
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
       ))}
     </div>
   );
