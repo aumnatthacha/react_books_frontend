9e388f19191c2744000875b8427ea574e09b3fc7
aumnatthacha <aummumdaeng@gmail.com>
2023-10-29 18:34:23 +0700

    Profile

--- src/pages/booksData.js
+++ src/pages/booksData.js
@@ -29,34 +29,5 @@ export const booksData = [
     type: 'Type for Book 4',
     imageURL: 'https://static.wixstatic.com/media/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg/v1/fill/w_209,h_296,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg',
   },
-  {
-    id: 6,
-    name: 'Book 3',
-    type: 'Type for Book 3',
-    imageURL: 'https://static.wixstatic.com/media/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg/v1/fill/w_209,h_296,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg',
-  },
-  {
-    id: 7,
-    name: 'Book 3',
-    type: 'Type for Book 3',
-    imageURL: 'https://static.wixstatic.com/media/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg/v1/fill/w_209,h_296,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg',
-  },
-  {
-    id: 8,
-    name: 'Book 4',
-    type: 'Type for Book 4',
-    imageURL: 'https://static.wixstatic.com/media/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg/v1/fill/w_209,h_296,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg',
-  },
-  {
-    id: 9,
-    name: 'Book 3',
-    type: 'Type for Book 3',
-    imageURL: 'https://static.wixstatic.com/media/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg/v1/fill/w_209,h_296,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg',
-  },
-  {
-    id: 10,
-    name: 'Book 4',
-    type: 'Type for Book 4',
-    imageURL: 'https://static.wixstatic.com/media/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg/v1/fill/w_209,h_296,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/936e61_66127639837742e68e78e65c1ca40964~mv2.jpg',
-  }
+
 ];
